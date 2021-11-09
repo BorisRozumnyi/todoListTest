75709106dc6c414a1396c8a1ad47ea9df19335b5
Boris <rozumniy.boris.work@gmail.com>
2021-11-09 15:19:43 +0200

    pased some mock data from backend to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,76 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+import './App.css';
+import logo from './logo.svg';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [error, setError] =
+    useState(null);
+  const [isLoaded, setIsLoaded] =
+    useState(false);
+  const [items, setItems] = useState(
+    []
+  );
 
-  React.useEffect(() => {
-    fetch("/api")
+  // Примечание: пустой массив зависимостей [] означает, что
+  // этот useEffect будет запущен один раз
+  // аналогично componentDidMount()
+  useEffect(() => {
+    fetch('/test-url/todos')
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then(
+        (result) => {
+          console.log(2, result);
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+        // чтобы не перехватывать исключения из ошибок в самих компонентах.
+        (error) => {
+          console.log(3);
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+  if (error) {
+    return (
+      <div>Ошибка: {error.message}</div>
+    );
+  } else if (!isLoaded) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <p>Загрузка...</p>
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {items && (
+            <ul>
+              {items.map((item) => (
+                <li key={item.title}>
+                  {item.title}{' '}
+                  {item.description}
+                </li>
+              ))}
+            </ul>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- testUrlController.js
+++ testUrlController.js
@@ -1,7 +1,26 @@
+const mockData = [
+  {
+    title: 'item 1',
+    description: 'some description 1',
+  },
+  {
+    title: 'item 2',
+    description: 'some description 2',
+  },
+  {
+    title: 'item 3',
+    description: 'some description 3',
+  },
+  {
+    title: 'item 4',
+    description: 'some description 4',
+  },
+];
+
 class testUrlController {
   async testAction(req, res) {
     try {
-      res.json('server worked');
+      res.json(mockData);
     } catch (error) {
       console.log(
         'error.message',
